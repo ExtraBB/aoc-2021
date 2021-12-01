4890ed6914e9ef9876e80cda90a156695b42f9aa
Bruno Carvalhal <bruno@email.com>
2021-12-01 16:35:37 +0100

    Working part 2

--- Main.hs
+++ Main.hs
@@ -13,5 +13,5 @@ day1 (x:y:xs) = fromEnum (y > x) + day1 (y:xs)
 day1 _ = 0
 
 day1part2 :: [Int] -> Int
-day1part2 xs@(u:v:w:x:y:z:_) = fromEnum ((x + y + z) > (u + v + w)) + day1part2 (tail xs)
+day1part2 xs@(w:x:y:z:_) = fromEnum ((x + y + z) > (w + x + y)) + day1part2 (tail xs)
 day1part2 _ = 0
