27eaba2667ea1a9c6ba9043ab4e04441e702aaa4
Bruno Carvalhal <bruno@email.com>
2021-12-01 06:15:45 -0800

    Update implementation

--- Main.hs
+++ Main.hs
@@ -5,8 +5,9 @@ import qualified MyLib (readLines, readInts)
 main :: IO ()
 main = do
   nums <- MyLib.readInts "input/day1"
-  putStrLn (show (day1_1 nums (0, maxBound)))
+  print (day1 nums)
 
-day1_1 :: [Int] -> (Int, Int) -> Int
-day1_1 [] (total, _) = total
-day1_1 (x:xs) (total, last) = day1_1 xs (if x > last then (total + 1, x) else (total, x))
+day1 :: [Int] -> Int
+day1 [] = 0
+day1 [_] = 0
+day1 (x:y:xs) = fromEnum (y > x) + day1 (y:xs)
