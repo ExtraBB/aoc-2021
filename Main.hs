d32151c91bab62b445aa590dab15197989bb5f6d
Bruno Carvalhal <bruno@email.com>
2021-12-01 22:23:47 +0100

    Improve main

--- Main.hs
+++ Main.hs
@@ -1,15 +1,20 @@
 module Main where
 
-import qualified InputHelpers (readInts)
+import qualified InputHelpers (readLines, readInts)
 import qualified Day1 (part1, part2)
 
 main :: IO ()
-main = do
-  nums <- InputHelpers.readInts "input/day1"
-  printDay 1 (Day1.part1 nums) (Day1.part2 nums)
+main = day 1
 
-printDay :: Show a => Int -> a -> a -> IO ()
-printDay n part1 part2 = do
+day :: Int -> IO ()
+day n = do
+  input <- InputHelpers.readLines $ "input/day" ++ show n
+  let (part1, part2) = runDay input n
   print $ "=== Results for day " ++ show n ++ " ==="
-  print $ "Part 1: " ++ show part1
-  print $ "Part 2: " ++ show part2
+  print $ "Part 1: " ++ part1
+  print $ "Part 2: " ++ part2
+
+runDay :: [String] -> Int -> (String, String)
+runDay lines n = case n of
+  1 -> (show $ Day1.part1 nums, show $ Day1.part2 nums) where nums = map read lines
+  _ -> ("None", "None")
