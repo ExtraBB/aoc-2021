54e9ff4e0c3bbcf09bb5c947cc29cde1faa8032e
Bruno Carvalhal <bruno@email.com>
2021-12-02 20:45:06 +0100

    Combine functions

--- days/Day2.hs
+++ days/Day2.hs
@@ -2,6 +2,7 @@ module Day2(part1, part2) where
 import Control.Monad.State
 
 data Direction = Forward Int | Down Int | Up Int
+data Rules = Part1 | Part2
 
 parseDirection :: String -> Direction
 parseDirection input = case words input of
@@ -10,32 +11,23 @@ parseDirection input = case words input of
      ("down"    :x:_) -> Down (read x)
      _ -> Forward 0
 
-move :: [Direction] -> State (Int, Int) Int
-move [] = do
-    (pos, depth) <- get
-    return (pos * depth)
-move (x:xs) = do
-    (pos, depth) <- get
-    case x of
-        Forward n   -> put (pos + n, depth)
-        Up n        -> put (pos, depth - n)
-        Down n      -> put (pos, depth + n)
-    move xs
-
 part1 :: [String] -> Int
-part1 lines = (evalState . move) (map parseDirection lines) (0,0)
-
-move2 :: [Direction] -> State (Int, Int, Int) Int
-move2 [] = do
-    (pos, depth, aim) <- get
-    return (pos * depth)
-move2 (x:xs) = do
-    (pos, depth, aim) <- get
-    case x of
-        Forward n   -> put (pos + n, depth + aim * n, aim)
-        Up n        -> put (pos, depth, aim - n)
-        Down n      -> put (pos, depth, aim + n)
-    move2 xs
+part1 lines = evalState (move Part1 lines) (0,0,0)
 
 part2 :: [String] -> Int 
-part2 lines = (evalState . move2) (map parseDirection lines) (0,0,0)
+part2 lines = evalState (move Part2 lines) (0,0,0)
+
+move :: Rules -> [String] -> State (Int, Int, Int) Int
+move _ [] = do
+    (pos, depth, aim) <- get
+    return (pos * depth)
+move rules (x:xs) = do
+    (pos, depth, aim) <- get
+    case direction of
+        Forward steps   -> put (pos + steps, depth + ax * aim * steps, aim)
+        Up      steps   -> put (pos, depth - steps * dx, aim - steps * ax)
+        Down    steps   -> put (pos, depth + steps * dx, aim + steps * ax)
+    move rules xs
+        where  
+            direction = parseDirection x
+            (dx, ax) = case rules of { Part1 -> (1,0);  Part2 -> (0,1)}
